b5cfdc54e7adad862d670d6986c14ce70667443f
eduribeirofaustino <eduribeirofaustino@gmail.com>
2023-06-03 13:56:21 -0300

    Tarefa da facul
    HTML, CSS e JAVASCRIPT

---
+++ Tarefa2/Tarefa.js
@@ -0,0 +1,12 @@
+/* document.write('Testando JavaScrpt...')*/
+
+/* console.log("testando o JS") */
+
+/* alert("Primeiro Alerta no Java Script")*/
+
+/* document.getElementById('titulo').innerHTML = 'Vamos Aprender JS '; */
+
+var x = 10;
+var y = 15;
+resultado = x * y;
+console.log('O resultado da soma é:'   +resultado)
